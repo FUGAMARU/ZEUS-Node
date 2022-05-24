5c161b752063c5dd01f88e15589c9412cb6c7cd8
FUGAMARU <gofugashi@gmail.com>
2022-05-24 15:19:42 +0900

    [Modify] 軽微な仕様変更

--- app.js
+++ app.js
@@ -1,67 +1,58 @@
-if(process.argv[2] === "dev" || process.argv[2] === "production"){
-	const app = require("express")()
-	const fs = require("fs")
-	const options = {
-		key: fs.readFileSync("/etc/letsencrypt/live/fugamaru.com/privkey.pem"),
-  		cert: fs.readFileSync("/etc/letsencrypt/live/fugamaru.com/fullchain.pem")
+const PORT = 1028
+const app = require("express")()
+const server = require("http").createServer(app)
+server.listen(PORT)
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"]
 	}
-	const server = process.argv[2] === "dev" ? require("http").createServer(app) : require("https").createServer(options, app)
-	server.listen(1028)
-	const io = require("socket.io")(server, {
-		cors: {
-			origin: "*",
-			methods: ["GET", "POST"]
+})
+let prevLog = "prevLog"
+
+const time = () => {
+	return `[${new Date().toLocaleString("ja-JP")}]`
+}
+
+console.log(`${time()} \x1b[37m\x1b[46mSocket.IO server has ready on port ${PORT}!\x1b[0m`)
+
+io.on("connection", (socket) => {
+	console.log(`${time()} \x1b[32m\x1b[1mNew Connection: \x1b[0m${socket.id}`)
+	socket.emit("welcome", "welcome")
+
+	socket.on("disconnect", (reason) => {
+		console.log(`${time()} \x1b[31m\x1b[1mDisconnected: \x1b[0m${socket.id} - ${reason}`)
+	})
+
+	socket.on("register", (res) => {
+		if(res !== undefined){
+			socket.join(res)
+			const str = `${time()} ${socket.id} joined room ${res}`
+			if(str !== prevLog){
+				console.log(str)
+				prevLog = str
+			}
 		}
 	})
-	const logger = require("log4js").getLogger()
-	logger.level = "all"
-	
-	const someFunction = () => {
-		console.log("Executed!")
-	}
-	
-	app.get("/", (req, res) => {
-		res.json({ status: 200 })
-		someFunction()
+
+	socket.on("sendMessage", (res) => {
+		console.log(`${time()} \x1b[36m\x1bㅤReceived Message:\x1b[0m\n${JSON.stringify(res)}`)
+		if(res.scope === "global"){
+			io.emit("receiveMessage", {
+				scope: "global",
+				userName: res.userName,
+				iconSrc: res.iconSrc,
+				datetime: `${new Date().getHours()}:${("00" + new Date().getMinutes()).slice(-2)}`,
+				message: res.message
+			})
+		}else if(res.scope === "class"){
+			io.in(res.classID).emit("receiveMessage", {
+				scope: "class",
+				userName: res.userName,
+				iconSrc: res.iconSrc,
+				datetime: `${new Date().getHours()}:${("00" + new Date().getMinutes()).slice(-2)}`,
+				message: res.message
+			})
+		}
 	})
-	
-	io.on("connection", (socket) => {
-		logger.info(`New Connection: ${socket.id}`)
-		socket.emit("welcome", "ようこそ")
-	
-		socket.on("disconnect", (reason) => {
-			logger.info(`Disconnected: ${socket.id} - ${reason}`)
-		})
-	
-		socket.on("register", (res) => {
-			if(res !== undefined){
-				socket.join(res)
-				console.log(`${socket.id}がルーム ${res} に参加しました`)
-			}
-		})
-	
-		socket.on("sendMessage", (res) => {
-			console.log(`新規メッセージ => ${JSON.stringify(res)}`)
-			if(res.scope === "global"){
-				io.emit("receiveMessage", {
-					scope: "global",
-					userName: res.userName,
-					iconSrc: res.iconSrc,
-					datetime: `${new Date().getHours()}:${("00" + new Date().getMinutes()).slice(-2)}`,
-					message: res.message
-				})
-			}else if(res.scope === "class"){
-				io.in(res.classID).emit("receiveMessage", {
-					scope: "class",
-					userName: res.userName,
-					iconSrc: res.iconSrc,
-					datetime: `${new Date().getHours()}:${("00" + new Date().getMinutes()).slice(-2)}`,
-					message: res.message
-				})
-			}
-		})
-	})
-}else{
-	console.log("起動オプションを指定してください。'dev' or 'production'")
-	process.exit()
-}
+})
