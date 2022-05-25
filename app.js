31ee1271bf06a4b44d504b8dd4aac912eef6de53
FUGAMARU <gofugashi@gmail.com>
2022-05-25 16:05:22 +0900

    [Add] Twitterのトレンドを取得してSocket.IOで配信する機能を実装

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
-const PORT = 1028
+require("dotenv").config()
+
+const Twitter = require("twitter")
+const client = new Twitter({
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+let trends = []
+const getTwitterTrends = () => {
+	client.get("trends/place", {id: 23424856}, (err, res) => {
+		if(!err){
+			const prevTrends = res[0]["trends"]
+	
+			const sortedTrends = Object.keys(prevTrends).map((key) => {
+				return prevTrends[key]
+			}).sort((a, b) => {
+				return (a.tweet_volume > b.tweet_volume) ? -1 : 1
+			})
+
+			trends = [{name: sortedTrends[0].name, url: sortedTrends[0].url}, {name: sortedTrends[1].name, url: sortedTrends[1].url}, {name: sortedTrends[2].name, url: sortedTrends[2].url}]
+			console.log(`${time()} \x1b[36m\x1b[1mTwitterトレンドを取得・更新しました\x1b[0m`)
+		}else{
+			console.log(`${time()} \x1b[31m\x1b[1mTwitterトレンドの取得エラーが発生しました\x1b[0m`)
+		}
+	})
+}
+
+getTwitterTrends()
+
+setInterval(() => {
+	getTwitterTrends()
+}, 900000)
+
 const app = require("express")()
 const server = require("http").createServer(app)
-server.listen(PORT)
+server.listen(process.env.PORT)
 const io = require("socket.io")(server, {
 	cors: {
 		origin: "*",
@@ -14,11 +49,11 @@ const time = () => {
 	return `[${new Date().toLocaleString("ja-JP")}]`
 }
 
-console.log(`${time()} \x1b[37m\x1b[46mSocket.IO server has ready on port ${PORT}!\x1b[0m`)
+console.log(`${time()} \x1b[37m\x1b[46mSocket.IO server has ready on port ${process.env.PORT}!\x1b[0m`)
 
 io.on("connection", (socket) => {
 	console.log(`${time()} \x1b[32m\x1b[1mNew Connection: \x1b[0m${socket.id}`)
-	socket.emit("welcome", "welcome")
+	socket.emit("welcome", trends)
 
 	socket.on("disconnect", (reason) => {
 		console.log(`${time()} \x1b[31m\x1b[1mDisconnected: \x1b[0m${socket.id} - ${reason}`)
@@ -54,5 +89,9 @@ io.on("connection", (socket) => {
 				message: res.message
 			})
 		}
+	})
+
+	socket.on("requestTrends", () => {
+		socket.emit("receiveTrends", trends)
 	})
 })
