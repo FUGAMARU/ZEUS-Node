076c4d6f44e0dcadc993b42a5487633a36a31896
FUGAMARU <gofugashi@gmail.com>
2022-05-26 12:24:40 +0900

    [Add] Twitterトレンドの最終取得時刻とトレンドのツイート数をデーターに含めるように

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const client = new Twitter({
 	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
 	access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
 	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
+})
 
 let trends = []
+let lastUpdate = ""
 const getTwitterTrends = () => {
 	client.get("trends/place", {id: 23424856}, (err, res) => {
 		if(!err){
@@ -20,7 +21,8 @@ const getTwitterTrends = () => {
 				return (a.tweet_volume > b.tweet_volume) ? -1 : 1
 			})
 
-			trends = [{name: sortedTrends[0].name, url: sortedTrends[0].url}, {name: sortedTrends[1].name, url: sortedTrends[1].url}, {name: sortedTrends[2].name, url: sortedTrends[2].url}]
+			trends = [{name: sortedTrends[0].name, url: sortedTrends[0].url, tweet_volume: sortedTrends[0].tweet_volume}, {name: sortedTrends[1].name, url: sortedTrends[1].url, tweet_volume: sortedTrends[1].tweet_volume}, {name: sortedTrends[2].name, url: sortedTrends[2].url, tweet_volume: sortedTrends[2].tweet_volume}]
+			lastUpdate = `${new Date().getHours()}:${("00" + new Date().getMinutes()).slice(-2)}`
 			console.log(`${time()} \x1b[36m\x1b[1mTwitterトレンドを取得・更新しました\x1b[0m`)
 		}else{
 			console.log(`${time()} \x1b[31m\x1b[1mTwitterトレンドの取得エラーが発生しました\x1b[0m`)
@@ -92,6 +94,6 @@ io.on("connection", (socket) => {
 	})
 
 	socket.on("requestTrends", () => {
-		socket.emit("receiveTrends", trends)
+		socket.emit("receiveTrends", {trends: trends, lastUpdate: lastUpdate})
 	})
 })
